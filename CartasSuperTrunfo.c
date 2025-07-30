abd64912302eb86ff54d6026ed10633dacf19abf
AdemirAssisHonorato <156374058+AdemirAssisHonorato@users.noreply.github.com>
2025-07-30 16:34:37 +0000

    DesafioSuperTrunfoNivelNovato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,127 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Trabalho de Nome: Ademir de Assis Honorato Gonçalves Junior, Curso: Engenharia de software
+// Atividade: Carta Super Trunfo nivel novato 
+/*
+Carta 1:
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+Estado: A
 
-    return 0;
+Código: A01
+
+Nome da Cidade: São Paulo
+
+População: 12325000
+
+Área: 1521.11 km²
+
+PIB: 699.28 bilhões de reais
+
+Número de Pontos Turísticos: 50
+
+ 
+
+Carta 2:
+
+Estado: B
+
+Código: B02
+
+Nome da Cidade: Rio de Janeiro
+
+População: 6748000
+
+Área: 1200.25 km²
+
+PIB: 300.50 bilhões de reais
+
+Número de Pontos Turísticos: 30
+
+*/
+
+int main(){
+//AQUI COMEÇA O CIDIGO.
+
+//AQUI FICA AS VARIAVEIS
+
+int codigo, ponto_turistico, codigo_, ponto_turistico_;    
+char estado[2], cidade[50];
+char estado_[2], cidade_[50];
+float populacao, area, pib, populacao_, area_, pib_;
+
+//AQUI INICIA OS DADOS DA CARTA 1
+    
+printf("Insira aqui os dados da Carta1\n");
+printf("Insira uma letra para a sua carta.\n");
+scanf("%s", estado);
+
+printf("Insira um codigo entre  01 e 04\n");
+scanf("%d", &codigo);
+
+printf("Insira o nome da cidade\n");
+scanf("%s", cidade);
+
+printf("Digite o numero da população\n");
+scanf("%d", &populacao);
+
+printf("Digite a área:\n");
+scanf("%i", &area);
+
+printf("PIB:\n");
+scanf("%i", &pib);
+
+printf("Digite quantidade de pontos turisticos, no Estado\n");
+scanf("%d", &ponto_turistico);
+
+//AQUI INICIA OS DADOS CARTA 2
+
+printf("\n"); //ESTA É UMA QUEBRA DE LINHA
+printf("Insira aqui os dados da Carta2\n");
+printf("Insira uma letra para a sua carta.\n");
+scanf("%s", estado_);
+
+printf("Insira um codigo entre  01 e 04\n");
+scanf("%d", &codigo_);
+
+printf("Insira o nome da cidade\n");
+scanf("%s", cidade_);
+
+printf("Digite o numero da população\n");
+scanf("%d", &populacao_);
+
+printf("Digite a área:\n");
+scanf("%i", &area_);
+
+printf("PIB:\n");
+scanf("%i", &pib_);
+
+printf("Digite quantidade de pontos turisticos, no Estado\n");
+scanf("%d", &ponto_turistico_);
+
+
+//AQUI INICIA O RESULTADO, SERÁ RESULTADO CARTA 1
+
+printf("Carta1\n");
+printf("Estado: %s\n", estado);
+printf("Codigo: %s %d\n", estado, codigo);
+printf("Nome da cidade: %s\n", cidade);
+printf("População: %d\n", populacao);
+printf("Área: %i km²\n", area);
+printf("PIB:%i R$\n", pib);
+printf("Pontos Turisticos:%d\n", ponto_turistico);
+
+//AQUI INICIA O RESULTADO CARTA 2
+
+printf("\n");
+printf("Carta2\n");
+printf("Estado: %s\n", estado_);
+printf("Codigo: %s %d\n", estado_, codigo_);
+printf("Nome da cidade: %s\n", cidade_);
+printf("População: %d\n", populacao_);
+printf("Área: %i km²\n", area_);
+printf("PIB:%i R$\n", pib_);
+printf("Pontos Turisticos:%d\n", ponto_turistico_);
+
+return 0;
+
 }
